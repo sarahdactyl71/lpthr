80762ce43fd52ccc5111fbae9e03f72e60deea39
Sarah Kirk <kirkse710@gmail.com>
2018-04-30 11:20:43 -0700

    changes raw_input to input

--- drills/your_quest.py
+++ drills/your_quest.py
@@ -1,5 +1,6 @@
-quest = raw_input("What is your quest?")
-name = raw_input("What is your name?")
-color = raw_input("What is your favorite color?")
+quest = input("What is your quest? ")
+name = input("What is your name? ")
+color = input("What is your favorite color? ")
 
-print("So your name is #{name}, your quest is #{quest}, and your favorite color is #{color}")
+
+print('So your name is {name}, your quest is {quest}, and your favorite color is {color}')
