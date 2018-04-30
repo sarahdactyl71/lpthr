0476e6c5da8ebec39f91806fe0b7003a86cf508d
Sarah Kirk <kirkse710@gmail.com>
2018-04-30 11:06:25 -0700

    starts writing your_quest.py

---
+++ drills/your_quest.py
@@ -0,0 +1,5 @@
+quest = raw_input("What is your quest?")
+name = raw_input("What is your name?")
+color = raw_input("What is your favorite color?")
+
+print("So your name is #{name}, your quest is #{quest}, and your favorite color is #{color}")
