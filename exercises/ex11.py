bf09e09aa0d46013f55edb5d4dc4629599b3c099
Sarah Kirk <kirkse710@gmail.com>
2018-04-30 15:03:05 -0700

    adds completed exercise 11

---
+++ exercises/ex11.py
@@ -0,0 +1,8 @@
+print("How old are you?", end=' ')
+age = input()
+print("How tall are you?", end=' ')
+height = input()
+print("What is your favotire color?", end=' ')
+color = input()
+
+print(f"So, you're {age} old, {height} tall and your favorite color is {color}?")
