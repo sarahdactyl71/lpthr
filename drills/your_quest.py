6ced4e96cd03e4bc1b717936eb9e774f6357b429
Sarah Kirk <kirkse710@gmail.com>
2018-04-30 11:32:26 -0700

    adds comments to your_quest.py

--- drills/your_quest.py
+++ drills/your_quest.py
@@ -2,5 +2,8 @@ quest = input("What is your quest? ")
 name = input("What is your name? ")
 color = input("What is your favorite color? ")
 
-
+#This method interpolates string with %s, kind of gross
 print('So your name is %s, your quest is %s, and your favorite color is %s!' %(name, quest, color))
+#This method intepolates the string with f in front of it and it's fucking beautiful,
+#prettier than ruby dare I say.
+print(f'So your name is {name}, your quest is {quest}, and your favorite color is {color}!')
