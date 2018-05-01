b3df157b91e01aa1182d942723d5eb9503fd2d7d
Sarah Kirk <kirkse710@gmail.com>
2018-05-01 16:35:21 -0700

    refactors exercise 16 a little bit

--- exercises/ex16.py
+++ exercises/ex16.py
@@ -20,14 +20,9 @@ line1 = input("line 1: ")
 line2 = input("line 2: ")
 line3 = input("line 3: ")
 
-print("I'm going to write ehses to the file.")
+print("I'm going to write these to the file.")
 
-target.write(line1)
-target.write("\n")
-target.write(line2)
-target.write("\n")
-target.write(line3)
-target.write("\n")
+target.write(f"{line1} \n {line2} \n {line3} \n")
 
 print("And finally, we close it.")
 target.close()
