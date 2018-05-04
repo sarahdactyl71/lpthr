9986ce539b06a7f6c89d29bbdf28436099165ec5
Sarah Kirk <kirkse710@gmail.com>
2018-05-04 14:27:52 -0700

    initial fixes to file

--- exercises/ex26.py
+++ exercises/ex26.py
@@ -3,27 +3,28 @@ from sys import argv
 print("How old are you?", end=' ')
 age = input()
 print("How tall are you?", end=' ')
-print("How much do you weigh?", end=' '
+height = input()
+print("How much do you weigh?", end=' ')
 weight = input()
 
 print(f"So, you're {age} old, {height} tall and {weight} heavy.")
 
 script, filename = argv
 
-txt = open(filenme)
+txt = open(filename)
 
-print("Here's your file {filename}:")
-print(tx.read())
+print(f"Here's your file {filename}:")
+print(txt.read())
 
 print("Type the filename again:")
 file_again = input("> ")
 
 txt_again = open(file_again)
 
-print(txt_again_read())
+print(txt_again.read())
 
 
-print('Let's practice everything.')
+print('Let\'s practice everything.')
 print('You\'d need to know \'bout escapes
       with \\ that do \n newlines and \t tabs.')
 
@@ -33,7 +34,7 @@ with logic so firmly planted
 cannot discern \n the needs of love
 nor comprehend passion from intuition
 and requires an explanation
-\n\t\twhere there is none.
+\n\t\t where there is none.
 """
 
 print("--------------)
@@ -41,8 +42,8 @@ print(poem)
 print(--------------")
 
 
-five = 10 - 2 + 3 -
-print(f"This should be five: {five}"
+five = 10 - 2 + 3
+print(f"This should be five: {five}")
 
 def secret_formula(started):
     jelly_beans = started * 500
@@ -52,7 +53,7 @@ def secret_formula(started):
 
 
 start_point = 10000
-beans, jars = secret_formula(start_point)
+beans, jars, crates = secret_formula(start_point)
 
 # remember that this is another way to format a string
 print("With a starting point of: {}".format(start_point))
@@ -69,7 +70,7 @@ print("We'd have {} beans, {} jars, and {} crates.".format(*formula))
 
 
 people = 20
-cates = 30
+cats = 30
 dogs = 15
 
 
