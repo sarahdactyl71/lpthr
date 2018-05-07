3df56bb98f280428c30aefd601942e3e4e8f4acf
Sarah Kirk <kirkse710@gmail.com>
2018-05-07 10:10:39 -0700

    updates ex33 to change while loop into function that is called at the bottom of the file

--- exercises/ex33.py
+++ exercises/ex33.py
@@ -1,16 +1,21 @@
-i = 0
-numbers  = []
+def while_loop(times, increment):
+    i = 0
+    numbers  = []
 
-while i < 6:
-    print(f"At the top i is {i}")
-    numbers.append(i)
+    while i < times:
+        print(f"At the top i is {i}")
+        numbers.append(i)
 
-    i += 1
+        i += increment
 
-    print("Numbers now: ", numbers)
-    print(f"At the bottom i is {i}")
+        print("Numbers now: ", numbers)
+        print(f"At the bottom i is {i}")
 
-print("The numbers: ")
+    print("The numbers: ")
 
-for num in numbers:
-    print(num)
+    for num in numbers:
+        print(num)
+
+print(f"""Here is the result of the while loop: \n
+{while_loop(6, 2)}
+""")
