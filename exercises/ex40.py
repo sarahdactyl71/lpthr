a346d83520d0d806054c1e7641d285308d2b6a14
Sarah Kirk <kirkse710@gmail.com>
2018-05-09 10:55:05 -0700

    adds completed exercise 40

--- exercises/ex40.py
+++ exercises/ex40.py
@@ -1,1 +1,26 @@
 # MODULES, CLASSES, AND OBJECTS
+
+class Song(object):
+
+    def __init__(self, lyrics):
+        self.lyrics = lyrics
+
+    def sing_me_a_song(self):
+        for line in self.lyrics:
+            print(line)
+
+yung_lean = Song(["So many lies that I've found",
+                  "Blood, heaven, I stick to the ground",
+                  "So many times I've realized",
+                  "What I seek for is right in front of my eyes"])
+
+the_surf = Song(["I shelter on the beach",
+                 "All alone",
+                 "You are the reach",
+                 "The surf comes every day",
+                 "Washes my creations away"])
+
+
+yung_lean.sing_me_a_song()
+
+the_surf.sing_me_a_song()
