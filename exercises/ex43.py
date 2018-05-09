bc76f20847b79d6fed5b4b8ab993e48e30b62e6b
Sarah Kirk <kirkse710@gmail.com>
2018-05-09 12:06:32 -0700

    fleshes out Scene class and Engine class

--- exercises/ex43.py
+++ exercises/ex43.py
@@ -1,16 +1,32 @@
 #Basic Object-Oriented Analysis and Design
+# Working from the top down
+
+from sys import exit
+from random import randint
+from textwrap import dedent
+
 class Scene():
 
     def enter(self):
-        pass
+        print("This scene is not yet configured.")
+        print("Subclass it and implement enter()")
+        exit(1)
 
 class Engine():
 
     def __init__(self, scene_map):
-        pass
+        self.scene_map = scene_map
 
     def play(self):
-        pass
+        current_scene = self.scene_map.opening_scene()
+        last_scene = self.scene_map.next_scene('finished')
+
+        while current_scene != last_scene:
+            next_scene_name = current_scene.enter()
+            current_scene = self.scene_map.next_scene(next_scene_name)
+
+        #be sure to print out the last scene
+        current_scene.enter()
 
 class Death(Scene):
 
@@ -35,7 +51,7 @@ class EscapePod(Scene):
 class Map():
 
     def __init__(self, start_scene):
-        pass
+        self.start_scene = start_scene
 
     def next_scene(self, scene_name):
         pass
