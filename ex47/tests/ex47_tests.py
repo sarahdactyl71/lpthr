64b265de8328eb5e0b0cb50bbf510f40772d01dd
Sarah Kirk <kirkse710@gmail.com>
2018-05-22 10:54:26 -0700

    adds test_room_paths to ex47_tests

--- ex47/tests/ex47_tests.py
+++ ex47/tests/ex47_tests.py
@@ -9,6 +9,16 @@ def test_room():
     assert_equal(gold.name, "GoldRoom")
     assert_equal(gold.paths, {})
 
+def test_room_paths():
+    center = Room("Center", "Test room in the center.")
+    north = Room("North", "Test room in the north.")
+    south =  Room("South", "Test roon in the south.")
+
+    center.add_paths({'north': north, 'south': south})
+
+    assert_equal(center.go('north'), north)
+    assert_equal(center.go('south'), south)
+
 def setup():
     print("SETUP!")
 
