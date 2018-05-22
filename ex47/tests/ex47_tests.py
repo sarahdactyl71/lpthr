7f6d8c75445f8d2d71fcb83dd03567f4d4e8ac5f
Sarah Kirk <kirkse710@gmail.com>
2018-05-22 10:51:24 -0700

    adds test to ex47_tests

--- ex47/tests/ex47_tests.py
+++ ex47/tests/ex47_tests.py
@@ -1,6 +1,14 @@
 from nose.tools import *
 import ex47.game import Room
 
+
+def test_room():
+    gold = Room("GoldRoom",
+                """THis room has gold in it you can grab. There's a
+                door to the north.""")
+    assert_equal(gold.name, "GoldRoom")
+    assert_equal(gold.paths, {})
+
 def setup():
     print("SETUP!")
 
