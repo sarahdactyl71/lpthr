a89ad96bcda3c307577789ffa8f95d70d03ccb61
Sarah Kirk <kirkse710@gmail.com>
2018-05-22 10:09:33 -0700

    adds file for ex46' '

--- scary_game/start_game.py
+++ scary_game/start_game.py
@@ -10,7 +10,7 @@ class Start():
         Through a series of escalading dares you and your friends find yourselves
         in a house that local legends would lead you to believe is haunted. \n
         You don't believe in ghosts or demons, but even so, as you walk through the
-        threshold of the front door, you find yourself a bit nervous. \n
+        threshold of the front door, you find yourself uncharacteristically nervous. \n
         Looking around you are in the foyer of a once great mansion. \n
         The years of neglect and decay have done their work on the home. \n
         You find it odd that none of the finer pieces like the candleabras
@@ -18,7 +18,7 @@ class Start():
         There is a suspicious looking stain in the persian carpet that you tell yourself
         is just mud and debris. \n
         You are the leader of your group. \n
-        Which way do you go first? \n 
+        Which way do you go first? \n
         The left side: It looks like it leads to a grand living room, with a disheveled
         grand piano and several pieces of fine handmade furniture. \n
         The right side: It looks like it leads to a kitchen. All the pots and pans are out, like
