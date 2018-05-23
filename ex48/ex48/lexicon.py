437f5694780cc9407171c1c13218a229f184e3e5
Sarah Kirk <kirkse710@gmail.com>
2018-05-23 09:31:15 -0700

    fixed syntax issue with importing file

---
+++ ex48/ex48/lexicon.py
@@ -0,0 +1,4 @@
+class Lexicon():
+
+    def scan(self, direction):
+        print("You did it")
--- ex48/tests/lexicon_tests.py
+++ ex48/tests/lexicon_tests.py
@@ -1,5 +1,5 @@
 from nose.tools import *
-from ex48 import lexicon
+from ex48.lexicon import Lexicon
 
 def test_directions():
     assert_equal(lexicon.scan("north"), [('direction', 'north')])
