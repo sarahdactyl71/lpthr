c066cec5fcf0c3c75312acb4946d83d52e68471b
Sarah Kirk <kirkse710@gmail.com>
2018-05-23 14:54:43 -0700

    finishes one story line for UpstairsRoom

--- scary_game/upstairs_room.py
+++ scary_game/upstairs_room.py
@@ -25,8 +25,8 @@ class UpstairsRoom():
         I am really tired of you kids coming into my home and disturbing my peace. This is breaking and
         entering you know. I have a mind to call the authorities on you." \n
         You try to stammer a reply, but before you can formulate that first word, the stranger
-        speaks again "Tell you what, I am old, and very lonely. You can either play me at a game of
-        chess or a hand of cards. Do this and I won't call the police." \n
+        speaks again "Tell you what, I haven't entertained visitors in a long time, and I am very lonely. Play me at a game of
+        chess, or a hand of cards, and I won't call the police." \n
         Which do you play? \n
         """)
 
@@ -34,21 +34,36 @@ class UpstairsRoom():
 
         print(f"""
         Confidence suddenly on your side, you say "{choice}" in a flat voice. From the side of the chair
-        you see an old hand guesture you forward.
+        you see a gloved hand guesture you forward.
         """)
 
         if "chess" in choice:
+            from dead import Dead
             print("""
             You approach from the right hand side. Sitting in the rocking chair, you see a frail old woman.
             She has a wool blanket resting over her legs, and is rocking gently back and forth on feet that you cannot see.
-            She is wearing a neat bun on the top of her head, and earrings so grand, and so large, you are surprised she
+            She is wearing a neat, white, bun on the top of her head, and earrings so grand, and so large, you are surprised she
             isn't toppling over. Despite her frailty she sits with an incredibly straight back. You try not to think that she
             might be stronger than she would appear. \n
             Without words she looks at you and nods at the board in front of her. Already set up, immaculately clean. The pieces
             gleam in the flickering fire. You sit in the armchair opposite the old woman. You convince yourself the armchair was always there.
-            The room itself, feels bigger somehow, but after looking around it appears that nothing has changed.
-            
+            The room itself, feels bigger somehow, but after looking around it appears that nothing has changed. \n
+            The woman makes the first move with a shaky hand. You follow up, first moving pawns to free up other
+            pieces on the board. After the woman moves her rook across the board to take one of your knights you notice her hands
+            are far less shaky. You move a piece to set up taking her rook and you see that your hands are losing composure. Nerves maybe?
+            Maybe it was a trick of the fire, but the woman's hair looks far darker than you first noticed. \n
+            You start to feel tired, at first a bit drowsy, but soon it is hard to keep your eyes open. You are hardly aware
+            that you are playing the game, moving pieces less and less carefully. \n
+            "Checkmate", you hear, nearly startled out of sleep. You look at the board and see that indeed it is checkmate.
+            A checkmate that you should have been able to avoid. You send your gaze to the old woman. However she isn't old anymore.
+            The hair on her head is thick and healthy, hands strong and certain in their movement. \n
+            You look down at your hands, frail, shakey, full of veins and skin like paper. You touch your face to find the elastic
+            has gone out of it, and you feel sagging flesh. Running your hands through your hair you find it is thin and brittle. \n
+            The woman hops up from her seat "That was a lovely game" she says. "I doubt we will play again, sorry". She walks out
+            of the room.
             """)
+            Dead().dead("""You try to get up and follow her, but you find that you are too weak. All you can do is rock back and forth in your chair
+            and warm your legs by the fire.""")
 
         elif "cards" in choice:
             print("""
