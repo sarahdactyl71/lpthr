0be986995a92491dad38d52ea90d2e1155174e8c
Sarah Kirk <kirkse710@gmail.com>
2018-05-25 10:05:19 -0700

    adds second story line to Basement()

--- scary_game/basement.py
+++ scary_game/basement.py
@@ -56,7 +56,7 @@ class Basement():
             ground will support you. After a few steps you find comfort in the earth
             pushing back at you and you guesture your friends to follow you deeper into
             the tunnel. \n
-            Keeping your right hand on the wall you walk, following the sound as it gets 
+            Keeping your right hand on the wall you walk, following the sound as it gets
             more and more defined. The tunnel gradually widens to a large room with
             several eroding pillars. You know the sound is coming from this room, but for
             whatever reason it is hard to triangulate. \n
@@ -73,13 +73,62 @@ class Basement():
 
             Dead.dead("""
             You look around and see now that everyone you were with is dead on the floor.
-            You hear walking and dragging sound, so very close to you now. You close your
+            You hear walking and dragging sound, so very close now. You close your
             eyes and wait. \n
             """)
 
         elif "trap" in choice:
             print("""
+            You look at Amy, and together you heave the trapdoor open. You have
+            Just enough time to move out of the way as the door crashing on the
+            ground where you just were. A coolness escapes from the chamber envelops
+            you all and the group lets out a shudder. \n
+            You look down and all you can see is darkness, with a rope ladder leading
+            the way down, eventually disappearing as the light has no more to travel. \n
+            You start making your way down. The ladder squeaks under your weight, but
+            after pushing yourself into it you determine that it should hold. Step by
+            step, you make your way down until your foot rests softly on the dirt floor. \n
+            One after the other your friends continue down in the same fashion. Now, it
+            is just Amy reamaining at the top of the entrance. \n
+            You call her name, and ask her to come down. She seems nervous, and
+            understandably so. After some convincing, Amy makes decends about halway
+            down the ladder when you notice something is wrong. \n
+            Like in a movie or a text based adventure game, you hear the sound of the
+            ropes fraying, and beginning to snap strands. You call Amy's name but she
+            seems to be paralyzed. You know that if you try and climb the ladder the
+            ropes with snap faster with you on it. You call her name again and she has
+            just enough time to give you a terrified look over her shoulder and the
+            ladder breaks. \n
+            You can only watch as the the ladder falls to the ground with Amy to follow.
+            You rush over to where Amy lay. She has blood running out of one ear
+            and she shows no signs of movement. You check her pulse and find that she,
+            in one sense of the phrase, is no longer with you. \n
+            As much as you would like to mourn, you have bigger concerns. As far as you
+            know, the ladder was the only way out of the pit. There is no cell phone
+            reception down here for you to call for help. However, after some inspection
+            you find that there seems to be channels of paths branching off from the
+            main chamber you find yourself in. The group decided that you should split up,
+            and should anyone find an exit they will come back and rescue everyone else.
+            You and two friends take the left most tunnel, and start to walk. Still using
+            your phones to light the way, you walk for a several hours. \n
+            You notice that the walls are occasionally adorned with simple paintings.
+            Most depicting people travelling in the direction that you are walking.
+            How ironic. \n
+            You hear a beeping noise and one of your friends informs you that their
+            phone is almost out of battery. After a few minutes, it turns off completely,
+            and you try not to notice that it is a bit darker. And hour later. the same thing
+            happens to your other friend. You gulp and lead the way carrying the only light
+            you have. After a while that too, blinks out. \n
+            Your only option is to keep a hand on the wall and continue wandering
+            blindly forward.
+            """)
 
+            Dead().dead("""
+            You're not sure how much time has passed, or how you lost your friends
+            along the way. Steps becoming harder now, you focus on moving one foot in
+            front of the other, until you cannot propel yourself forward. You tell
+            yourself it would be okay to sit and rest. You'll get up at any moment and
+            keep walking. Any moment. 
             """)
 
         else:
