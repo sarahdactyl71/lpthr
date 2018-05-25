9edbb6ca7ed75e1ee38432a0abaaea9a8982ead6
Sarah Kirk <kirkse710@gmail.com>
2018-05-25 10:46:40 -0700

    begins writing story for Hallway()

---
+++ scary_game/hallway.py
@@ -0,0 +1,15 @@
+class Hallway():
+
+    def hallway(self):
+        print("""
+        Maybe you are feeling bold, or perhaps you are feeling a bit rash, but
+        you decide to follow the voice that was coming from the hallway. You make
+        your way forward, apprehensive of what you might find. \n
+        There is a mirror at the end of the hall, only visible by the moonlight
+        coming through one of the windows. In the mirror you see the reflection of
+        a little girl. The era of her clothing looks like the era of the house, and
+        similarly, in the same degree of disrepair. \n
+        You walk closer to the mirror, directly in front of it, and address the child.
+        You ask her what she is doing in the mirror, and what happened to her.
+        She tells you that  
+        """)
