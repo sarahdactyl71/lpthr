aca1c8c01c8643068003929e3a1518d144a2bd33
Sarah Kirk <kirkse710@gmail.com>
2018-05-29 11:58:54 -0700

    adds tests for numbers

--- ex48/ex48/lexicon.py
+++ ex48/ex48/lexicon.py
@@ -10,19 +10,19 @@ lexicon = {
     'in': 'stop',
     'of': 'stop',
     'bear': 'noun',
-    'princess': 'noun'
+    'princess': 'noun',
+    '1234': 'number',
+    '3': 'number',
+    '91234': 'number',
     }
 
 def scan(phrase):
     words = phrase.split()
     output = []
-
     for word in words:
         checked_word = convert_numbers(word)
-
         if word in lexicon.keys():
             output.append((lexicon[word], checked_word))
-
     return output
 
 
