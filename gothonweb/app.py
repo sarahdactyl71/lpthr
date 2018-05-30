f56522cf1bf2c03c84f5883f431f2a5214322950
Sarah Kirk <kirkse710@gmail.com>
2018-05-30 12:08:25 -0700

    adds more functionality with args to app.py

--- gothonweb/app.py
+++ gothonweb/app.py
@@ -4,19 +4,20 @@ from flask import request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/hello')
 def index():
     #these are like params in url if none are there it will say
     #"Hello nobody", if you have something like http://localhost:5000/hello?name=Frank
     # It will say "Hello Frank"
     name = request.args.get('name', 'Nobody')
+    greet = request.args.get('greet', 'Hello')
 
     if name:
-        greeting = f"Hello, {name}"
+        greeting = f"{greet}, {name}"
     else:
         greeting = "Hello World"
 
-    retunr render_template("index.html", greeting=greeting)
+    return render_template("index.html", greeting=greeting)
 
 if __name__ == "__main__":
     app.run()
