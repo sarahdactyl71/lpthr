4e539131db33042e539cdd0c7aa606d3827ee9ff
Sarah Kirk <kirkse710@gmail.com>
2018-05-31 15:41:02 -0700

    starts fleshing out tests in app_tests

---
+++ gothonweb/tests/app_tests.py
@@ -0,0 +1,12 @@
+from nose.tools import *
+from app import app
+
+app.config['TESTING'] = True
+web = app.test_client()
+
+def test_index():
+    rv = web.get('/', follow_redirects=True)
+    assert_equal(rv.status_code, 404)
+
+    rv = web.get('/hello', follow_redirects=True)
+    assert_equal(rv.status_code, 200)
